571cc7b1c695cf2ba69299fda3e4a4f48d07f358
Eddy Verbruggen <eddyverbruggen@gmail.com>
2015-04-12 14:23:15 +0000

    Added some fun demos

--- Flashlight plugin demo/scripts/demo.js
+++ Flashlight plugin demo/scripts/demo.js
@@ -30,6 +30,52 @@
             }
         },
 
+        sos: function () {
+            if (!this.checkSimulator()) {
+              // OK, LOL, this is a very poor SOS implementation, but here we go to produce the ...---... effect:
+	            window.plugins.flashlight.switchOn(); // .
+              setTimeout(window.plugins.flashlight.switchOff,  400);
+              setTimeout(window.plugins.flashlight.switchOn,   700); // ..
+              setTimeout(window.plugins.flashlight.switchOff, 1000);
+              setTimeout(window.plugins.flashlight.switchOn,  1300); // ...
+              setTimeout(window.plugins.flashlight.switchOff, 1600);
+              setTimeout(window.plugins.flashlight.switchOn,  1900); // ...-
+              setTimeout(window.plugins.flashlight.switchOff, 2700);
+              setTimeout(window.plugins.flashlight.switchOn,  3100); // ...--
+              setTimeout(window.plugins.flashlight.switchOff, 3900);
+              setTimeout(window.plugins.flashlight.switchOn,  4300); // ...---
+              setTimeout(window.plugins.flashlight.switchOff, 5100);
+              setTimeout(window.plugins.flashlight.switchOn,  5500); // ...---.
+              setTimeout(window.plugins.flashlight.switchOff, 5800);
+              setTimeout(window.plugins.flashlight.switchOn,  6100); // ...---..
+              setTimeout(window.plugins.flashlight.switchOff, 6400);
+              setTimeout(window.plugins.flashlight.switchOn,  6700); // ...---...
+              setTimeout(window.plugins.flashlight.switchOff, 7000);
+            }
+        },
+
+        stroboscope: function () {
+            if (!this.checkSimulator()) {
+              var inter = setInterval(
+                function() {
+                  window.plugins.flashlight.toggle()
+                }, 100
+              );
+
+              setTimeout(function() {
+                clearInterval(inter);
+                // let's make sure it's off
+  	            window.plugins.flashlight.switchOff();
+              }, 7000);
+            }
+        },
+
+        toggleAfter: function(ms) {
+          setTimeout(function() {
+	            window.plugins.flashlight.toggle(this.onSuccess, this.onError);
+          }, ms);
+        },
+
         checkSimulator: function() {
             if (window.navigator.simulator === true) {
                 alert('This plugin is not available in the simulator.');
